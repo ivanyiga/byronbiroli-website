ce94c80406408924b0bb1c5bcfbf047d56f97c37
Ivan Yiga <ivanatresyn@gmail.com>
2026-04-20 00:01:57 +0300

    Optimized for vercel deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    preset: 'vercel'
+  },
+  routeRules: {
+    '/': { prerender: true }
+  },
 
   app: {
     head: {
